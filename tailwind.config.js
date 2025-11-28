fbc0acc42e23e04ea6b81dcfd93c6fe58fb85dd7
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2025-11-28 11:49:26 -0300

    feat: add tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      // Copiar do tailwind.config.ts do React
+    },
+  },
+  plugins: [],
+}
