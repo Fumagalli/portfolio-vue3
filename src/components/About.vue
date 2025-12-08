d9e4c1ec16e2d518744fc4319155fafa53450111
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2025-12-08 15:12:07 -0300

    chore: add empty files to migrate

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<!-- React Hook equivalente -->
+<!-- const { t } = useTranslation(); -->
+
+<!-- Vue 3 Composition API -->
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<!-- Binding de eventos -->
+<!-- React: onClick={handler} -->
+<!-- Vue: @click="handler" -->
+
+<!-- Conditional rendering -->
+<!-- React: {condition && <Component />} -->
+<!-- Vue: <Component v-if="condition" /> -->
+
+<!-- Loop -->
+<!-- React: {items.map(item => <Component :key={item.id} />)} -->
+<!-- Vue: <Component v-for="item in items" :key="item.id" /> -->
